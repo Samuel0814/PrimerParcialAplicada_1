e7774aea1f01e64d054a75cb1ed837bb7babbdbb
Samuel Alexander Reyes Diaz <samuel_reyes1@ucne.edu.do>
2018-06-06 16:24:40 -0400

    Guardar test, y estaba tratando de solucionar el error del Ado.net

---
+++ PrimerParcialTests/BLL/EstudianteBLLTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrimerParcial.BLL;
+using PrimerParcial.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrimerParcial.BLL.Tests
+{
+    [TestClass()]
+    public class EstudianteBLLTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            bool paso;
+            Estudiantes Estudiante = new Estudiantes();
+            Estudiante.GrupoId = 10;
+            Estudiante.Descripcion = "Que tal";
+            Estudiante.Fecha = DateTime.Now;
+            Estudiante.Cantidad = 30;
+            Estudiante.Grupos = 6;
+            paso = BLL.EstudianteBLL.Guardar(Estudiante);
+            Assert.AreEqual(paso, true);
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
